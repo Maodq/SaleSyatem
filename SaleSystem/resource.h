06ca175aedec1bea835daa9d05c2bf752838bfe3
riddle <754561543@qq.com>
2020-06-25 15:26:45 +0800

    测试

--- SaleSystem/resource.h
+++ SaleSystem/resource.h
@@ -9,18 +9,21 @@
 #define IDI_ICON_RE                     310
 #define IDI_ICON_WIN                    311
 #define DIALOG_LOGIN                    312
+#define IDD_DIALOG1                     314
 #define IDC_EDIT1                       1000
 #define IDC_EDIT2                       1001
 #define IDC_BUTTON1                     1002
 #define IDC_BUTTON2                     1003
+#define IDC_BUTTON3                     1004
+#define IDC_BUTTON4                     1005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        314
+#define _APS_NEXT_RESOURCE_VALUE        316
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
